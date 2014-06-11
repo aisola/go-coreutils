fedcd67ca254ba0f9981add90d95953958b6036e
Abram C. Isola <abram@isola.mn>
2014-06-11 18:07:04 -0500

    Add arch command

---
+++ arch/arch.go
@@ -0,0 +1,50 @@
+//
+// arch.go (go-coreutils) 0.1
+// Copyright (C) 2014, The GO-Coreutils Developers.
+// 
+// Written By: Abram C. Isola
+//
+package main
+
+import "flag"
+import "fmt"
+import "os"
+import "runtime"
+
+const (
+	help_text string = `
+    Usage: arch
+    
+    print the system architecture
+
+        --help        display this help and exit
+        --version     output version information and exit
+    `
+	version_text = `
+    arch (go-coreutils) 0.1
+
+    Copyright (C) 2014, The GO-Coreutils Developers.
+    This program comes with ABSOLUTELY NO WARRANTY; for details see
+    LICENSE. This is free software, and you are welcome to redistribute 
+    it under certain conditions in LICENSE.
+`
+)
+
+func main() {
+	help := flag.Bool("help", false, help_text)
+	version := flag.Bool("version", false, version_text)
+	flag.Parse()
+
+	if *help {
+		fmt.Println(help_text)
+		os.Exit(0)
+	}
+
+	if *version {
+		fmt.Println(version_text)
+		os.Exit(0)
+	}
+
+	fmt.Println(runtime.GOARCH)
+
+}
