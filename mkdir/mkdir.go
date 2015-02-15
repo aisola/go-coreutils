696aaf3875379dff5baee6e88709afc1bc274989
Marco don Kaulea <DonMarco42@gmail.com>
2015-02-15 10:28:44 +0100

    Changes output format to be identical to gnu mkdir
    Fixes #39

--- mkdir/mkdir.go
+++ mkdir/mkdir.go
@@ -10,6 +10,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 const (
@@ -18,7 +19,7 @@ const (
 
         --help        display this help and exit
         --version     output version information and exit
-        --parents     create parent directory/directories as 
+        --parents     create parent directory/directories as
                       needed, do nothing if already existing
         --verbose     print a message for each created directory
   `
@@ -28,13 +29,13 @@ const (
 
     Copyright (C) 2014, The GO-Coreutils Developers.
     This program comes with ABSOLUTELY NO WARRANTY; for details see
-    LICENSE. This is free software, and you are welcome to redistribute 
+    LICENSE. This is free software, and you are welcome to redistribute
     it under certain conditions in LICENSE.
   `
 
 	parents_text = `
     Create parent directory/directories as needed for a path. If any
-    already exist, do nothing. 
+    already exist, do nothing.
   `
 
 	usage_text = `usage: mkdir [-parents, -verbose] directory ...`
@@ -48,6 +49,43 @@ var (
 	parents = flag.Bool("parents", false, parents_text)
 	verbose = flag.Bool("verbose", false, verbose_text)
 )
+
+func extend(slice []string, element string) []string {
+	if len(slice) == cap(slice) {
+		newSlice := make([]string, len(slice), 2*cap(slice))
+		copy(newSlice, slice)
+		slice = newSlice
+	}
+	n := len(slice)
+	slice = slice[0 : n+1]
+	slice[n] = element
+	return slice
+}
+
+func getAllPaths(dir string) []string {
+	slice := make([]string, 0, 10)
+	i := 0
+	_, err := os.Stat(dir)
+	for err != nil {
+		if dir == "." || dir == "/" {
+			break
+		}
+		slice = extend(slice, dir)
+		i += 1
+		dir = filepath.Dir(dir)
+		_, err = os.Stat(dir)
+	}
+	return slice
+}
+
+func printAllPaths(slice []string) {
+	i := len(slice)
+	for i > 0 {
+		i -= 1
+		fmt.Printf("mkdir: created directory `%s'\n", slice[i])
+
+	}
+}
 
 func main() {
 	flag.Parse()
@@ -68,13 +106,15 @@ func main() {
 	}
 
 	for i := 0; i < flag.NArg(); i++ {
+
 		if *parents {
+			paths := getAllPaths(flag.Arg(i))
 			mkdirAllError := os.MkdirAll(flag.Arg(i), os.ModePerm)
 
 			if mkdirAllError != nil {
 				fmt.Println(mkdirAllError)
 			} else if *verbose {
-				fmt.Printf("%s\n", flag.Arg(i))
+				printAllPaths(paths)
 			}
 		} else {
 			mkdirError := os.Mkdir(flag.Arg(i), os.ModePerm)
@@ -85,5 +125,6 @@ func main() {
 				fmt.Printf("mkdir: created directory '%s'\n", flag.Arg(i))
 			}
 		}
+
 	}
 }
